8f494e419abfc93c82b7a6ea8cdcf84df97e13d5
Sherzod Mamadaliev <smamadaliev@ya.ru>
2018-04-02 14:44:52 +0300

    lib.h - заголовочный файл

---
+++ labs/09/lib.h
@@ -0,0 +1,13 @@
+#define MAXLENGTH 128
+#define MINWORDS 2
+#define MAXWORDS 20
+#define MAXROWS 20
+
+#ifndef lib_h
+#define lib_h
+
+void input_str(char arr[][MAXLENGTH], int n);
+void variant_29(char arr[][MAXLENGTH], int n);
+void output_str(char arr[][MAXLENGTH], int n);
+
+#endif
